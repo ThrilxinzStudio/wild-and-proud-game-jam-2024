f67d4497001cfcd97dedb7c07eb77584ea76bd07
LynnePlex <38146775+LynnePLex@users.noreply.github.com>
2024-06-23 10:28:17 -0500

    main scene set

--- game/autoloads/r.gd
+++ game/autoloads/r.gd
@@ -2,11 +2,14 @@
 extends "res://addons/popochiu/engine/interfaces/i_room.gd"
 
 # classes ----
+const PRMainScene := preload("res://game/rooms/main_scene/room_main_scene.gd")
 # ---- classes
 
 # nodes ----
+var MainScene: PRMainScene : get = get_MainScene
 # ---- nodes
 
 # functions ----
+func get_MainScene() -> PRMainScene: return super.get_runtime_room("MainScene")
 # ---- functions
 
