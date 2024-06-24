50a4a252b7bb47b76b2b8b234c8a8b09124058b6
LynnePlex <38146775+LynnePLex@users.noreply.github.com>
2024-06-24 04:08:18 -0500

    Added Charecters
    Adding some characters to the scene.
    The player
    Deshawn

--- game/autoloads/c.gd
+++ game/autoloads/c.gd
@@ -2,11 +2,17 @@
 extends "res://addons/popochiu/engine/interfaces/i_character.gd"
 
 # classes ----
+const PCPlayer := preload("res://game/characters/player/character_player.gd")
+const PCDashawn := preload("res://game/characters/dashawn/character_dashawn.gd")
 # ---- classes
 
 # nodes ----
+var Player: PCPlayer : get = get_Player
+var Dashawn: PCDashawn : get = get_Dashawn
 # ---- nodes
 
 # functions ----
+func get_Player() -> PCPlayer: return super.get_runtime_character("Player")
+func get_Dashawn() -> PCDashawn: return super.get_runtime_character("Dashawn")
 # ---- functions
 
