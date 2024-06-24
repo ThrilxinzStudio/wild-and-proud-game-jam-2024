b1833b308894c85764f8395b7b8f63b547d577bc
LynnePlex <38146775+LynnePLex@users.noreply.github.com>
2024-06-24 13:20:51 -0500

    First scene done
    Created second scene
    Changed sprite colors

--- game/autoloads/r.gd
+++ game/autoloads/r.gd
@@ -3,13 +3,16 @@ extends "res://addons/popochiu/engine/interfaces/i_room.gd"
 
 # classes ----
 const PRNewBeginning := preload("res://game/rooms/new_beginning/room_new_beginning.gd")
+const PRFourCorners := preload("res://game/rooms/four_corners/room_four_corners.gd")
 # ---- classes
 
 # nodes ----
 var NewBeginning: PRNewBeginning : get = get_NewBeginning
+var FourCorners: PRFourCorners : get = get_FourCorners
 # ---- nodes
 
 # functions ----
 func get_NewBeginning() -> PRNewBeginning: return super.get_runtime_room("NewBeginning")
+func get_FourCorners() -> PRFourCorners: return super.get_runtime_room("FourCorners")
 # ---- functions
 
